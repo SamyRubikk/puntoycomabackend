7c7629a82ca1913d9ae48cef54b9d04d392bd719
SamyRubikk <speedcuberstugfa@gmail.com>
2025-08-27 15:24:19 -0600

    Update server.ts
    Transfer

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
+// config/server.ts
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  app: { keys: env.array('APP_KEYS') },
+
+  // 👇 Habilita transferencias remotas y usa tu salt
+  transfer: {
+    token: { salt: env('TRANSFER_TOKEN_SALT') },
+    remote: { enabled: true },
   },
 });
